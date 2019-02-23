b5468d468acb7b472cba5efe44b39f5c51f4c4ab
Abdelrahman Ismail <abdelrahman3d@gmail.com>
2019-02-23 11:30:21 +0300

    style: remove comments

--- src/js/components/line.js
+++ src/js/components/line.js
@@ -45,21 +45,3 @@ export default class Line extends Component {
     tooltip: 'Line separator'
   }
 }
-
-// addLine: {
-//   element: 'dropdown',
-//   items: [
-//     '<hr class="align-line">',
-//     '<hr class="align-line is-dashed">',
-//     '<hr class="align-line is-dotted">',
-//     '<hr class="align-line is-double">',
-//     '<hr class="align-line is-dots">',
-//     '<hr class="align-line is-bold">',
-//     '<hr class="align-line is-bold is-dashed">',
-//     '<hr class="align-line is-bold is-dotted">',
-//     '<hr class="align-line is-bold is-double">'
-//   ],
-//   icon: 'insertLine',
-//   func: 'addHTML',
-//   tooltip: 'Add line'
-// },
